cd3e0d60448d8da48cc299e84765ab9f788f2376
SepehrNoey <sepehr.nk.81@gmail.com>
2023-12-16 13:03:29 +0330

    feat: get, create, update in basket handler added.

--- internal/infra/http/handler/basket.go
+++ internal/infra/http/handler/basket.go
@@ -1,10 +1,21 @@
 package handler
 
 import (
+	"errors"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/SepehrNoey/Cart-Service/internal/domain/model"
 	"github.com/SepehrNoey/Cart-Service/internal/domain/repository/basketrepo"
+	"github.com/SepehrNoey/Cart-Service/internal/infra/http/request"
 	"github.com/labstack/echo/v4"
+)
+
+const (
+	Pending   string = "PENDING"
+	Completed        = "COMPLETED"
 )
 
 type BasketHandler struct {
@@ -34,7 +45,71 @@ func (bh *BasketHandler) Get(c echo.Context) error {
 }
 
 func (bh *BasketHandler) Create(c echo.Context) error {
+	var req request.BasketCreate
 
+	if err := c.Bind(&req); err != nil {
+		return echo.ErrBadRequest
+	}
+	if err := req.CreateValidate(); err != nil {
+		return echo.ErrBadRequest // maybe return validation error
+	}
+
+	// now, we create a new basket
+	id := rand.Uint64() % 1_000_000
+	now := time.Now()
+	if err := bh.repo.Create(c.Request().Context(), model.Basket{
+		ID:        id,
+		CreatedAt: now,
+		UpdatedAt: now,
+		Data:      req.Data,
+		State:     Pending,
+	}); err != nil {
+		if errors.Is(err, basketrepo.ErrBasketIDDuplicate) {
+			return echo.ErrBadRequest
+		}
+		return echo.ErrInternalServerError
+	}
+
+	return c.JSON(http.StatusCreated, id)
+}
+
+func (bh *BasketHandler) Update(c echo.Context) error {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	var req request.BasketUpdate
+	if err = c.Bind(&req); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	baskets := bh.repo.Get(c.Request().Context(), basketrepo.GetCommand{ID: &id})
+	if len(baskets) == 0 {
+		return echo.ErrNotFound
+	}
+	// we have found the basket to update
+	basket := baskets[0]
+
+	// now, validation
+	if err = req.UpdateValidate(); err != nil {
+		return echo.ErrBadRequest // maybe return validation error
+	}
+
+	err = bh.repo.Update(c.Request().Context(), basketrepo.GetCommand{ID: &id}, model.Basket{
+		ID:        basket.ID,
+		CreatedAt: basket.CreatedAt,
+		UpdatedAt: time.Now(),
+		Data:      req.Data,
+		State:     req.State,
+	})
+
+	if req.State == Completed {
+		// TODO
+	}
+
+	// maybe change here to return the whole basket
+	return c.JSON(http.StatusOK, id)
 }
 
 func (bh *BasketHandler) Register(g *echo.Group) {
--- internal/infra/http/request/basket.go
+++ internal/infra/http/request/basket.go
@@ -10,11 +10,24 @@ type BasketCreate struct {
 	Data []byte `json:"data,omitempty" valid:"length(0|2048),optional"`
 }
 
-func (bc BasketCreate) Validate() error {
+func (bc BasketCreate) CreateValidate() error {
 	if _, err := govalidator.ValidateStruct(bc); err != nil {
 		return fmt.Errorf("create request validation failed %w", err)
 	}
 
 	return nil
 
+}
+
+type BasketUpdate struct {
+	Data  []byte `json:"data,omitempty" valid:"length(0|2048),optional"`
+	State string `josn:"state,omitempty" valid:"(COMPLETED|PENDING),optional"`
+}
+
+func (bu BasketUpdate) UpdateValidate() error {
+	if _, err := govalidator.ValidateStruct(bu); err != nil {
+		return fmt.Errorf("update request validation failed %w", err)
+	}
+
+	return nil
 }
